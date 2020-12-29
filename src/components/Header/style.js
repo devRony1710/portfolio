b6a65643f7002db52d482fbd8f14450384da8fcb
Rony Antolinez <rony1710.dev@gmail.com>
2020-12-29 17:27:54 -0500

    fix bug en header component

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -2,7 +2,9 @@ import styled from 'styled-components';
 
 export const HeaderWrapper = styled.header`
   height: 10vh;
+  width: 100%;
   display: flex;
+  position: fixed;
   justify-content: space-between;
   align-items: center;
   background: #051626;
